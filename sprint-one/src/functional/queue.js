1a266f4fa7ed528e97395fb2a24ebe637c28580a
kartikvempati <kvempati11@gmail.com>
2015-11-18 15:29:05 -0800

    implement queue with functional initialization

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function() {
   var someInstance = {};
+  var queueSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
@@ -7,12 +8,30 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[queueSize] = value;
+    queueSize++;
   };
 
   someInstance.dequeue = function() {
+    var dequeued = storage[0];
+    delete storage[0];
+
+    for (var i = 1; i <= queueSize - 1; i++) {
+      storage[i-1] = storage[i];
+    }
+    delete storage[queueSize - 1];
+    if (queueSize > 0) {
+      queueSize--;
+    }
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return queueSize;
+  };
+
+  someInstance.storage = function() {
+    return storage;
   };
 
   return someInstance;
