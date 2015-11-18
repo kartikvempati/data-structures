1ee9f64b9e9ab270089a736d6dc459dbe30cdc07
kartikvempati <kvempati11@gmail.com>
2015-11-18 14:44:20 -0800

    implement stack with functional initialization

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function() {
   var someInstance = {};
+  var stackSize = 0;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackSize] = value;
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    var value = storage[stackSize - 1];
+    delete storage[stackSize - 1];
+
+    if (stackSize > 0){
+      stackSize--;
+    }
+    return value;
   };
 
   someInstance.size = function() {
+    return stackSize;
   };
 
   return someInstance;
