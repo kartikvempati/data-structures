fe40102dd53d8d333611f9eb62eb2785ab26ae81
kartikvempati <kvempati11@gmail.com>
2015-11-18 17:19:25 -0800

    implement queue and stack in pseudoclassical initialization

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var newQueue = {};
-  newQueue.sizeProp = 0;
+  newQueue.queueSize = 0;
   newQueue.storage = {};
   extend(newQueue, queueMethods);
   return newQueue;
@@ -17,25 +17,25 @@ var extend = function(to, from){
 var queueMethods = {};
 
 queueMethods.size = function() {
-  return this.sizeProp;
+  return this.queueSize;
 };
 
 queueMethods.enqueue = function(value) {
-  this.storage[this.sizeProp] = value;
-  this.sizeProp++;
+  this.storage[this.queueSize] = value;
+  this.queueSize++;
 };
 
 queueMethods.dequeue = function() {
   var dequeued = this.storage[0];
   delete this.storage[0];
-  
-  for (var i = 1; i <= this.sizeProp - 1; i++) {
+
+  for (var i = 1; i <= this.queueSize - 1; i++) {
     this.storage[i-1] = this.storage[i];
   } 
   
-  delete this.storage[this.sizeProp - 1];
-  if (this.sizeProp > 0 ) {
-    this.sizeProp--;
+  delete this.storage[this.queueSize - 1];
+  if (this.queueSize > 0 ) {
+    this.queueSize--;
   }
   return dequeued;
 };
