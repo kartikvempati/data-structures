53985fdd3e1a7600e5d40fe7b9c468536077a2ad
kartikvempati <kvempati11@gmail.com>
2015-11-18 16:37:43 -0800

    implement queue and stack in pseudoclassical initialization

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.queueSize = 0;
+  this.storage = {};
 };
 
+var queueMethods = {};
 
+Queue.prototype.size = function() {
+  return this.queueSize;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.queueSize] = value;
+  this.queueSize++;
+};
+
+Queue.prototype.dequeue = function() {
+  var dequeued = this.storage[0];
+  delete this.storage[0];
+
+  for (var i = 1; i <= this.queueSize - 1; i++) {
+    this.storage[i-1] = this.storage[i];
+  } 
+  
+  delete this.storage[this.queueSize - 1];
+  if (this.queueSize > 0 ) {
+    this.queueSize--;
+  }
+  return dequeued;
+};
