db6aaa83e6fbca337d610e9c8a8a8c69486e0c4c
kartikvempati <kvempati11@gmail.com>
2015-11-19 21:14:12 -0800

    Complete Hash Tables.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,29 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  if(this._storage.get(index) !== undefined){
+    this._storage.get(index)[k] = v;
+  } 
+  else {
+    var newObj = {};
+    newObj[k] = v;
+    this._storage.set(index, newObj);  
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage.get(index)[k];
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.each( function(value, i, collection) {
+    if (index === i){
+      delete collection[i][k];
+    }
+  });
 };
 
 
@@ -22,5 +37,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
