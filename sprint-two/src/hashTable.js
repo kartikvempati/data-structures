f63d075d98ec27c31c9df208940c9dc9ec521197
kartikvempati <kvempati11@gmail.com>
2015-11-20 12:23:25 -0800

    refactor hashTable to use an array to handle collisions.

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,26 +9,49 @@ HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
 
   if(this._storage.get(index) !== undefined){
-    this._storage.get(index)[k] = v;
+    var arr = this._storage.get(index)
+    var collision = true;
+    for (var i = 0; i < arr.length; i++){
+      if(arr[i][0] === k){
+        arr[i][1] = v;
+        collision = false;
+      }
+    } 
+    if (collision === true){
+      arr.push([k,v]);
+    }
   } 
   else {
-    var newObj = {};
-    newObj[k] = v;
-    this._storage.set(index, newObj);  
+    var arr = [];
+    arr.push([k,v]);
+    this._storage.set(index, arr);  
   }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(index)[k];
+  var arr = this._storage.get(index);
+
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i][0] === k){
+      return arr[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   this._storage.each( function(value, i, collection) {
     if (index === i){
-      delete collection[i][k];
-    }
+      var arr = collection[i];
+      var removed;
+      for (var j = 0; j < arr.length; j++){
+        if (arr[j][0] === k){
+          removed = j;
+        }
+      }
+      collection[i] = arr.slice(0, removed).concat(arr.slice(removed+1));
+    }  
   });
 };
 
@@ -36,7 +59,8 @@ HashTable.prototype.remove = function(k) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
- insert: O(1)
- retrieve: O(1)
- remove: O(n)
+ n= length of the array | c = number tuples
+ insert: O(1), if no collision, O(c) if collision
+ retrieve: O(1) if c == 1, o(c) if more than one tuple
+ remove: O(n*c)
  */
