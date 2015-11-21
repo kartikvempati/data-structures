06afe13c852a3de8afd38689d318449b22aacab6
kartikvempati <kvempati11@gmail.com>
2015-11-20 22:18:20 -0800

    Add tested doublyLinkedList data structure.

--- sprint-two/spec/doublyLinkedListSpec.js
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -15,4 +15,40 @@ describe('doublyLinkedList', function() {
     expect(doublyLinkedList.removeHead).to.be.a("function");
     expect(doublyLinkedList.contains).to.be.a("function");
   });
+
+  it('should designate a new tail when new nodes are added', function() {
+    doublyLinkedList.addToTail(4);
+    expect(doublyLinkedList.tail.value).to.equal(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.tail.value).to.equal(5);
+  });
+
+  it('should designate a new head when nodes are removed', function() {
+    doublyLinkedList.addToTail(4);
+    doublyLinkedList.addToTail(5);
+    expect(doublyLinkedList.head.value).to.equal(4);
+    doublyLinkedList.removeHead();
+    expect(doublyLinkedList.head.value).to.equal(5);
+  });
+
+  it('should return the value of the removed head', function() {
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.addToTail(6);
+    expect(doublyLinkedList.removeHead()).to.equal(5);
+    expect(doublyLinkedList.removeHead()).to.equal(6);
+  });
+
+  it('should contain values that were added', function() {
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.addToTail(6);
+    expect(doublyLinkedList.contains(5)).to.equal(true);
+    expect(doublyLinkedList.contains(7)).to.equal(false);
+  });
+
+  it('should not contain values that were removed', function() {
+    doublyLinkedList.addToTail(5);
+    doublyLinkedList.addToTail(6);
+    doublyLinkedList.removeHead();
+    expect(doublyLinkedList.contains(5)).to.equal(false);
+  });
 });
--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -1,16 +1,63 @@
 var DoublyLinkedList = function() {
-  this.head = null;
-  this.tail = null;
+  var list = {};
+  list.head = null;
+  list.tail = null;
+
+
+  list.addToTail = function(value){
+    var node = Node(value);
+    //node.prev = list.tail;
+    if (list.tail !== null) {
+      list.tail.next = node;
+      node.prev = list.tail;
+    }
+    list.tail = node;
+    if(list.head === null){
+      list.head = node;
+    }
+  };
+
+  list.removeHead = function(){
+    var removedHead = list.head;
+
+    if (list.head.next === null){
+      list.head = null;
+      list.tail = null;
+    } else {
+      list.head = list.head.next;
+    }
+
+    return removedHead.value;
+  };
+
+  list.contains = function(target){
+    var current = list.head;
+    var result = false;
+
+    while(current !== null) {
+      if (current.value === target){
+        result = true;
+      }
+      current = current.next
+    }
+    return result;
+  };
+
+  return list;
 }
+/*
+ * Complexity: What is the time complexity of the above functions?
+ addToTail: O(1)
+ removeHead: O(1)
+ contains: O(n)
+ */
 
-DoublyLinkedList.prototype.addToTail = function(){
+ var Node = function(value) {
+  var node = {};
 
-};
+  node.value = value;
+  node.next = null;
+  node.prev = null;
 
-DoublyLinkedList.prototype.removeHead = function(){
-
-};
-
-DoublyLinkedList.prototype.contains = function(){
-
+  return node;
 };
