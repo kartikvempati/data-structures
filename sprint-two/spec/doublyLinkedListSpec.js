5f213864b5d8304c3dd2ab661ed3ab39a5461578
kartikvempati <kvempati11@gmail.com>
2015-11-20 18:14:24 -0800

    add addToTail, removeHead and contains

--- sprint-two/spec/doublyLinkedListSpec.js
+++ sprint-two/spec/doublyLinkedListSpec.js
@@ -8,5 +8,11 @@ describe('doublyLinkedList', function() {
   it ('should have a head and tail', function() {
     expect(doublyLinkedList).to.have.property("head");
     expect(doublyLinkedList).to.have.property("tail");
-  })
+  });
+
+  it('should have methods named "addToTail", "removeHead", and "contains"', function(){
+    expect(doublyLinkedList.addToTail).to.be.a("function");
+    expect(doublyLinkedList.removeHead).to.be.a("function");
+    expect(doublyLinkedList.contains).to.be.a("function");
+  });
 });
--- sprint-two/src/doublyLinkedList.js
+++ sprint-two/src/doublyLinkedList.js
@@ -2,3 +2,15 @@ var DoublyLinkedList = function() {
   this.head = null;
   this.tail = null;
 }
+
+DoublyLinkedList.prototype.addToTail = function(){
+
+};
+
+DoublyLinkedList.prototype.removeHead = function(){
+
+};
+
+DoublyLinkedList.prototype.contains = function(){
+
+};
